8896e2ae4c094c2796fd81564222a978b56f53bc
SaranHasnor <SaranHasnor@users.noreply.github.com>
2015-02-27 18:08:06 +0100

    Ajout du script de tracking et mise en place d'une structure de réception des données dans Unity

---
+++ Unity/RVRA/Assets/OscListenerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class OscListenerScript : MonoBehaviour
+{
+	[SerializeField]
+	private string _oscAddress;	// Osc Address we want to listen to
+
+	public abstract void ReceiveOscMessage(OscMessage message);
+
+	private void RegisterOscListener()
+	{
+		OscMessageHandlerScript.mainHandler.AddListener(this, _oscAddress);
+	}
+
+	void Start()
+	{
+		RegisterOscListener();
+	}
+}
---
+++ Unity/RVRA/Assets/OscMessageHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OscMessageHandlerScript : MonoBehaviour
+{
+	public string remoteIP = "127.0.0.1";
+	public int listeningPort = 7000;
+
+	private Osc _handler;
+	private Dictionary<string, OscListenerScript> _listeners;
+
+	private static OscMessageHandlerScript _mainHandler;
+	public static OscMessageHandlerScript mainHandler
+	{
+		get
+		{
+			return _mainHandler;
+		}
+	}
+
+	public void AddListener(OscListenerScript listener, string address)
+	{
+		_listeners.Add(address, listener);
+	}
+
+	private void AllMessageHandler(OscMessage oscMessage)
+	{
+		string msgAddress = oscMessage.Address;
+
+		if (_listeners.ContainsKey(msgAddress))
+		{
+			_listeners[msgAddress].ReceiveOscMessage(oscMessage);
+		}
+	}
+	
+	void Start()
+	{
+		_mainHandler = this;
+
+		_listeners = new Dictionary<string,OscListenerScript>();
+
+		// init OSC packet receiver
+		UDPPacketIO udp = GetComponent<UDPPacketIO>();
+		udp.init(remoteIP, 0, listeningPort);
+		_handler = GetComponent<Osc>();
+		_handler.init(udp);
+		_handler.SetAllMessageHandler(AllMessageHandler);
+	}
+}
