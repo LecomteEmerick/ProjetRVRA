f90de675c896ec385df1bb7298659125a20e41ce
Lecomte <emerick.lecomte@gmail.com>
2015-02-28 01:09:02 +0100

    fixes

---
+++ Unity/RVRA/Assets/Script/RelaunchBallInputScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RelaunchBallInputScript : MonoBehaviour {
+
+    public ballManagerScript script;
+
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            script.LaunchBall();
+        }
+	}
+}
